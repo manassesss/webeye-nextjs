a3bef06740a8f28af9434f109f65d3774f7367a8
Samuel <42661697+SamuelSSan28@users.noreply.github.com>
2021-10-23 00:20:57 -0300

    add novos componentes

---
+++ components/Sucesso/index.js
@@ -0,0 +1,62 @@
+import {
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Input,
+  Button,
+  Icon,
+  useColorModeValue,
+  createIcon,
+} from '@chakra-ui/react';
+
+export default function Sucess() {
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      py={12}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack
+        boxShadow={'2xl'}
+        bg={useColorModeValue('white', 'gray.700')}
+        rounded={'xl'}
+        p={10}
+        spacing={8}
+        align={'center'}>
+        <Heading
+          textTransform={'uppercase'}
+          fontSize={'5xl'}
+        >
+          🎉
+        </Heading>
+        <Stack align={'center'} spacing={2}>
+          <Heading
+            textTransform={'uppercase'}
+            fontSize={'3xl'}
+            color={useColorModeValue('gray.800', 'gray.200')}>
+            Concluído com Sucesso
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.500'}>
+            Obrigado por participar do experimento!
+          </Text>
+        </Stack>
+        <Stack spacing={4} direction={{ base: 'column', md: 'row' }} w={'full'}>
+          <Button
+            as="a"
+            href="/"
+            bg={'blue.400'}
+            rounded={'full'}
+            color={'white'}
+            flex={'1 0 auto'}
+            _hover={{ bg: 'blue.500' }}
+            _focus={{ bg: 'blue.500' }}>
+            Voltar a tela inicial
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+}
+
--- pages/captura.js
+++ pages/captura.js
@@ -2,36 +2,47 @@ import React, { useState, useEffect, useRef } from 'react';
 import Calibracao from '../components/Calibracao'
 import ShowVideos from '../components/ShowVideos'
 import Loading from '../components/Loading'
+import Sucesso from '../components/Sucesso'
 import styles from '../styles/Captura.module.css'
 
 export default function Captura() {
     const reactPlayerRef = useRef();
     const webcamRef = React.useRef(null);
     const [isFullScreen, setsIsFullScreen] = useState(false);
+    const [finalizado, setFinalizado] = useState(false);
     const [posicaoAtual, setPosicaoAtual] = useState(0);
     const [inicio, setIniciar] = useState(false)
     const [loading, setLoading] = useState(false)
+    const [loadingMessage, setLoadingMessage] = useState(0)
+
+    const mensagens_inicio = ["Preaparando o ambiente ...", "Carregando os videos ..."];
+    const mensagens_final = ["Salvando imagens ...", "Enviando para o servidor ..."];
 
     return (
         <div className={styles.container2} >
             {
-                (!isFullScreen || !inicio) ?
-                    <Calibracao
-                        isFullScreen={isFullScreen}
-                        setsIsFullScreen={setsIsFullScreen}
-                        webcamRef={webcamRef}
-                        setIniciar={setIniciar}
-                        setLoading={setLoading}/>
+                finalizado ?
+                    <Sucesso />
                     :
-                loading ?
-                        <Loading 
-                            text={"Aguarde um instante..."}/>
-                    :
-                        <ShowVideos
+                    (!isFullScreen || !inicio) ?
+                        <Calibracao
                             isFullScreen={isFullScreen}
-                            posicaoAtual={posicaoAtual}
-                            setPosicaoAtual={setPosicaoAtual}
-                            setFinishLoad={setLoading} />
+                            setsIsFullScreen={setsIsFullScreen}
+                            webcamRef={webcamRef}
+                            setIniciar={setIniciar}
+                            setLoading={setLoading} />
+                        :
+                        loading ?
+                            <Loading
+                                messages={loadingMessage == 0 ? mensagens_inicio : mensagens_final} />
+                            :
+                            <ShowVideos
+                                isFullScreen={isFullScreen}
+                                posicaoAtual={posicaoAtual}
+                                setPosicaoAtual={setPosicaoAtual}
+                                setFinalizado={setFinalizado}
+                                setLoadingMessage={setLoadingMessage}
+                                setFinishLoad={setLoading} />
             }
         </div>
 
